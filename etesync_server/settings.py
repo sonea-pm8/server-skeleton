3afbe0f657d9e32894df10445aa3129d32ac0893
Nora <nora.widdecke@tu-bs.de>
2019-06-08 15:25:51 +0200

    set log level to INFO

--- etesync_server/settings.py
+++ etesync_server/settings.py
@@ -153,6 +153,22 @@ JOURNAL_API_PERMISSIONS = (
         'rest_framework.permissions.IsAuthenticated',
         )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 # Cors
 CORS_ORIGIN_ALLOW_ALL = True
 
